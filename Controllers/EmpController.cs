a837e4ecc86f59a55c9cf2f2d435c9512c0879a0
divyanshkumar99 <divyanshkumar99bly@gmail.com>
2022-03-10 13:56:36 +0530

    My Project

---
+++ Controllers/EmpController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dockerapi.Models;
+namespace Dockerapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            var Emplist = new List<Employee>()
+            {
+                new Employee() {EName="Divyansh",EId=100,EDept="Development",EPno=1234567890},
+                new Employee() {EName="DKumar",EId=102,EDept="Security",EPno=1234567890},
+                new Employee() {EName="Dkumar2",EId=104,EDept="Management",EPno=1234567890},
+                new Employee() {EName="DKumar3",EId=106,EDept="Transport",EPno=1234567890},
+                new Employee() {EName="DKumar4",EId=108,EDept="FoodSupply",EPno=1234567890}
+            };
+            return Ok(Emplist);
+        }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dockerapi.Models
+{
+    public class Employee
+    {
+        public string EName { get; set; }
+        public int EId { get; set; }
+        public string EDept { get; set; }
+        public double EPno { get; set; }
+    }
+}
